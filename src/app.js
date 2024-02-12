73562b61b542803616a89b6e7bd374239e52fd40
awellsbiz <awellsbiz@gmail.com>
2024-02-12 12:52:15 -0800

    ouline

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-const button = document.getElementById('start');
+// const aboutContent = document.getElementById('about-link');
 
-function hello() {
-  console.log('Hello, world! I am here bitch!');
+function openModal() {
+  const showModal = document.querySelector('.modal');
+  showModal.style.display = 'block';
+  console.log('Modal clicked');
+}
+
+function closeModal() {
+  const closeModal = document.querySelector('.modal');
+  closeModal.style.display = 'none';
+  console.log('Modal closed');
 }
